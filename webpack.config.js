d080c33f966fee17e1136154a8f04ac027235583
bentomli <ben.tomlin@finn.no>
2016-11-02 16:30:23 +0100

    Use single quotes

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
 const path = require('path');
 
 const PATHS = {
-  app: path.join(__dirname, "app"),
-  dist: path.join(__dirname, "dist")
+  app: path.join(__dirname, 'app'),
+  dist: path.join(__dirname, 'dist')
 };
 
 module.exports = {
   entry: {
-    javascript: ["babel-polyfill", path.resolve(PATHS.app, "scripts", "main.js")],
-    html: path.resolve(PATHS.app, "index.html"),
-    css: path.resolve(PATHS.app, "styles", "main.css")
+    main: ['babel-polyfill', path.resolve(PATHS.app, 'scripts', 'main.js')],
+    html: path.resolve(PATHS.app, 'index.html'),
+    css: path.resolve(PATHS.app, 'styles', 'main.css')
   },
 
   output: {
     path: PATHS.dist,
-    filename: "./assets/scripts/bundle.js"
+    filename: './assets/scripts/bundle.js'
   },
 
   resolve: {
     extensions: ['', '.js', '.jsx', '.json'],
-    root: path.join(PATHS.app, "scripts")
+    root: path.join(PATHS.app, 'scripts')
   },
 
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        loaders: ["babel-loader"],
+        loaders: ['babel-loader'],
         exclude: /node_modules/,
         include: PATHS.app
       },
       {
         test: /\.css$/,
-        loaders: ["css"],
+        loaders: ['css'],
         exclude: /node_modules/,
         include: PATHS.app
       },
       {
         test: /\.html$/,
-        loader: "file?name=[name].[ext]",
+        loader: 'file?name=[name].[ext]',
         exclude: /node_modules/,
         include: PATHS.app
       }
