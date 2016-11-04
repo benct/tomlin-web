e891a4e776ac3bf97e45c5c6f3812bd07feaf426
bentomli <ben.tomlin@finn.no>
2016-11-04 22:01:04 +0100

    Separate entry js

---
+++ app/scripts/entry.js
@@ -0,0 +1,12 @@
+// CSS resources
+import '../styles/main.css';
+
+// Static assets
+import '../403.html'
+import '../404.html'
+import '../humans.txt'
+import '../robots.txt'
+import '../favicon.ico'
+
+// Javascript app entry
+import './main.js'
