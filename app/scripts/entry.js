6514a0dcd96138d6efaedbae4ef9d9ea494f48a8
bentomli <ben.tomlin@finn.no>
2016-11-06 15:28:04 +0100

    Import multiple files correctly

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -1,14 +1,18 @@
-// CSS resources
-import '../styles/main.css';
-import '../styles/error.css';
+// Import all files in a folder
+function requireAll(requireContext) {
+    return requireContext.keys().map(requireContext);
+}
 
 // Static assets
 import '../.htaccess';
 import '../humans.txt';
 import '../robots.txt';
 
+// CSS resources
+requireAll(require.context('../styles', true, /^\.\//));
+
 // Favicon assets
-require("../images/icon/" + regexphack);
+requireAll(require.context('../images/icon', true, /^\.\//));
 
 // Javascript app entry
 import './main.js';
