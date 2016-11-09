d8ab24b9629eee44b8ca03385db0a4402db64e60
bentomli <ben@tomlin.no>
2016-11-09 23:22:59 +0100

    Remove test text

--- app/scripts/views/about.jsx
+++ app/scripts/views/about.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 export default class About extends React.Component {
     render() {
         return (
-            <div className=''>
-                about page test
-            </div>
+            <div className=''></div>
         );
     }
 }
