20bc71a0f05ee35c4e8d4a0bd6e24cdf3eb40d7f
bentomli <ben@tomlin.no>
2016-11-10 00:50:48 +0100

    Dummy text on about page

--- app/scripts/views/about.jsx
+++ app/scripts/views/about.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class About extends React.Component {
     render() {
         return (
-            <div className=''></div>
+            <div className="text">There's really not that much interesting here...</div>
         );
     }
 }
