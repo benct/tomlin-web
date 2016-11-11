0ac491c7258e8f4beabcf0f60cbce7d444580bc4
bentomli <ben@tomlin.no>
2016-11-11 20:18:15 +0100

    Update about page

--- app/scripts/views/about.jsx
+++ app/scripts/views/about.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-export default class About extends React.Component {
-    render() {
-        return (
-            <div className="text">There's really not that much interesting here...</div>
-        );
-    }
+export default function About() {
+    return (
+        <div className="text">There's really not that much interesting here...<br/>This site is developed using <a href="https://facebook.github.io/react/" target="_blank">React</a> in conjunction with <a href="https://webpack.github.io/" target="_blank">Webpack</a>, <a href="https://babeljs.io/" target="_blank">Babel</a> and <a href="https://www.npmjs.com/" target="_blank">Node/NPM</a>, for the purpose of learning and testing it out. For more information about me, feel free to follow the social links at the bottom of the page.</div>
+    );
 }
