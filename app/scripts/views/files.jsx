9441f6be0b34ecd89cc0211467ae70755b2f8265
bentomli <ben@tomlin.no>
2016-11-17 18:19:07 +0100

    Refactor file list and fix fetch function

--- app/scripts/views/files.jsx
+++ app/scripts/views/files.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import fetch from '../api/api.js'
+import Filelist from '../components/filelist.jsx';
+import fetchContent from '../api/api.js';
 
 export default class Files extends React.Component {
     constructor(props) {
@@ -8,34 +9,32 @@ export default class Files extends React.Component {
         this.state = {
             cwd: '',
             content: [],
+            status: 200
         };
+        this.handleClick = this.handleClick.bind(this);
     }
 
     componentDidMount() {
-        this.refreshContent();
+        this.refreshContent(this.state.cwd);
     }
 
-    refreshContent() {
-        fetch({ action: 'ls', path: this.state.cwd })
-            .then((data) => this.setState({ content: data.content }))
+    refreshContent(cwd) {
+        fetchContent({ action: 'ls', path: cwd })
+            .then((data) => this.setState({ content: data.content, status: data.status }))
             .catch((error) => console.log('Request failed:', error));
     }
 
     changeDirectory(dirname) {
         let dir = this.state.cwd;
         if (dirname === '..') {
-            if (dir !== '') {
-                dir = dir.substring(0, dir.lastIndexOf('/'));
-            }
+            if (dir === '')
+                return;
+            dir = dir.substring(0, dir.lastIndexOf('/'));
         } else {
-            if (dir === '') {
-                dir = dirname;
-            } else {
-                dir += '/' + dirname;
-            }
+            dir += (dir === '' ? '' : '/') + dirname;
         }
         this.setState({ cwd: dir });
-        this.refreshContent();
+        this.refreshContent(dir);
     }
 
     openFile(path) {
@@ -56,37 +55,11 @@ export default class Files extends React.Component {
         }
     }
 
-    renderItem(content) {
-        return (
-            <tr>
-                <td><img className="fileicon" src={require('../../images/file/' + content.icon)}/></td>
-                <td className="filename"><span onClick={() => this.handleClick(content)}>{content.short}</span></td>
-                <td className="fileinfo monospace hide-lt480 rightify"><span>{content.size}</span></td>
-                <td className="fileinfo monospace hide-lt768"><span>{content.perms}</span></td>
-                <td className="fileinfo monospace hide-lt600"><span>{content.date}</span></td>
-            </tr>
-        );
-    }
-
     render() {
         return (
             <div>
                 <div onClick={() => this.changeDirectory('..')}>UP</div>
-                <table className="filesystem">
-                    <thead>
-                        <tr>
-                            <th>&nbsp;</th>
-                            <th className="leftify">Filename</th>
-                            <th className="hide-lt480">Size</th>
-                            <th className="hide-lt768">Permissions</th>
-                            <th className="hide-lt600">Uploaded</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.content.length ?
-                            this.state.content.map((item, i) => this.renderItem(item, i)) : (<tr><td>Empty</td></tr>)}
-                    </tbody>
-                </table>
+                <Filelist content={this.state.content} handleClick={this.handleClick} />
             </div>
         );
     }
