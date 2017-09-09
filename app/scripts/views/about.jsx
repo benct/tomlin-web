a60f2221f04fd46de36bd7a8397deb54e66940a3
bentomli <ben@tomlin.no>
2017-09-09 23:33:11 +0200

    Add copyright notice and refactor text slightly

--- app/scripts/views/about.jsx
+++ app/scripts/views/about.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 
 export default function About() {
     return (
-        <div className="text">There's really not that much interesting here...<br/>This site is developed using <a href="https://facebook.github.io/react/" target="_blank">React</a> in conjunction with <a href="https://webpack.github.io/" target="_blank">Webpack</a>, <a href="https://babeljs.io/" target="_blank">Babel</a> and <a href="https://www.npmjs.com/" target="_blank">Node/NPM</a>, for the purpose of learning and testing it out. For more information about me, feel free to follow the social links at the bottom of the page.</div>
+        <div className="text">
+            There's really not that much interesting here...<br/>This site is developed
+            using <a href="https://facebook.github.io/react/" target="_blank">React</a> in conjunction
+            with <a href="https://webpack.github.io/" target="_blank">Webpack</a>, <a href="https://babeljs.io/" target="_blank">Babel</a>
+            and <a href="https://www.npmjs.com/" target="_blank">Node/NPM</a>, for the purpose of learning
+            and testing it out. For more information about me, feel free to follow the social links
+            at the bottom of the page.<br/><br/>Ben Tomlin &copy; 2017
+        </div>
     );
 }
