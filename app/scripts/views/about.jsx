bab01911be047b30e06f56228abad3cd10376e64
bentomli <ben@tomlin.no>
2017-09-10 02:06:32 +0200

    Update about text slightly

--- app/scripts/views/about.jsx
+++ app/scripts/views/about.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 export default function About() {
     return (
         <div className="text">
-            There's really not that much interesting here...<br/>This site is developed
+            There's really not that much interesting here...<br/>This site was developed a while back
             using <a href="https://facebook.github.io/react/" target="_blank">React</a> in conjunction
-            with <a href="https://webpack.github.io/" target="_blank">Webpack</a>, <a href="https://babeljs.io/" target="_blank">Babel</a>
-            and <a href="https://www.npmjs.com/" target="_blank">Node/NPM</a>, for the purpose of learning
-            and testing it out. For more information about me, feel free to follow the social links
-            at the bottom of the page.<br/><br/>Ben Tomlin &copy; 2017
+            with <a href="https://webpack.github.io/" target="_blank">Webpack</a>,&nbsp;
+            <a href="https://babeljs.io/" target="_blank">Babel</a> and <a href="https://www.npmjs.com/" target="_blank">Node/NPM</a>,
+            for the purpose of learning and testing it out. For more information about me, feel free to
+            follow the social links at the bottom of the page.<br/><br/>Ben Tomlin &copy; 2017
         </div>
     );
 }
