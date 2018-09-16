c9120646167c8480eae2fd9f0f47f5d2ae8efc8e
benct <ben@tomlin.no>
2018-09-16 13:56:42 +0200

    Change about text

--- app/scripts/components/home/about.jsx
+++ app/scripts/components/home/about.jsx
@@ -1,32 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function About() {
     return (
         <div className="wrapper text">
-            {`There's really not that much interesting here...`}
+            There&apos;s really not that much interesting here...
             <br />
             <br />
-            {`This site was developed a while back using `}
-            <a href="https://facebook.github.io/react/" target="_blank" rel="noopener noreferrer">
-                React
-            </a>
-            {`, built with `}
-            <a href="https://webpack.github.io/" target="_blank" rel="noopener noreferrer">
-                Webpack
-            </a>
-            {`, `}
-            <a href="https://babeljs.io/" target="_blank" rel="noopener noreferrer">
-                Babel
-            </a>
-            {` and `}
-            <a href="https://www.npmjs.com/" target="_blank" rel="noopener noreferrer">
-                Node/NPM
-            </a>
-            {`, for the purpose of learning and testing it out. `}
-            {`For more information about me, feel free to follow the social links at the bottom of this page.`}
+            This site was developed a while back using React, for the purpose of learning and testing new features, and acquiring basic
+            knowledge about Webpack, Babel, Node and other web development technologies. For more information about me, click&nbsp;
+            <Link to="/me">here</Link>
+            &nbsp;or feel free to follow the social links at the bottom of this page.
             <br />
             <br />
-            Version 2.5.0
+            <a href="https://github.com/benct/tomlin-web" target="_blank" rel="noopener noreferrer">
+                Version 2.5.0
+            </a>
         </div>
     );
 }
