468a3ec18698e19c96785cc47d51a42dddf6c3e1
benct <ben@tomlin.no>
2018-09-18 23:43:22 +0200

    Fix hot module replacement

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -24,3 +24,7 @@ requireAll(require.context('../content', true, /^\.\//));
 
 // Render app
 ReactDOM.render(<App />, document.getElementById('root'));
+
+if (module.hot) {
+    module.hot.accept();
+}
