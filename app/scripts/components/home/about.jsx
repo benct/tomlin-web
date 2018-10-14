1d02cdb761e0706c49f9189f98442891bd064c43
benct <ben@tomlin.no>
2018-10-14 17:49:35 +0200

    Update about text

--- app/scripts/components/home/about.jsx
+++ app/scripts/components/home/about.jsx
@@ -4,13 +4,15 @@ import { Link } from 'react-router-dom';
 export default function About() {
     return (
         <div className="wrapper text">
-            There&apos;s really not that much interesting here...
+            Well <span className="strong">hello</span> there!
+            <br />
+            Unfortunately, there&apos;s not that much interesting here...
             <br />
             <br />
             This site was developed a while back using React, for the purpose of learning and testing new features, and acquiring basic
             knowledge about Webpack, Babel, Node and other web development technologies. For more information about me, click&nbsp;
             <Link to="/me">here</Link>
-            &nbsp;or feel free to follow the social links at the bottom of this page.
+            &nbsp;or feel free to follow the social links at the bottom of the page.
             <br />
             <br />
             <a href="https://github.com/benct/tomlin-web/blob/master/CHANGELOG.md" target="_blank" rel="noopener noreferrer">
