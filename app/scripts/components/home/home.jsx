db8c6666f842e21037291944317adcff23574a75
benct <ben@tomlin.no>
2018-10-14 17:50:59 +0200

    Move about text to home component

--- app/scripts/components/home/home.jsx
+++ app/scripts/components/home/home.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Countdown from './countdown.jsx';
 import Quote from './quote.jsx';
-import About from './about.jsx';
 
 export default function Home() {
     return (
         <>
-            <About />
+            <div className="wrapper text">
+                Well <span className="strong">hello</span> there!
+                <br />
+                Unfortunately, there&apos;s not that much interesting here...
+                <br />
+                <br />
+                This site was developed a while back using React, for the purpose of learning and testing new features, and acquiring basic
+                knowledge about Webpack, Babel, Node and other web development technologies. For more information about me, click&nbsp;
+                <Link to="/me">here</Link>
+                &nbsp;or feel free to follow the social links at the bottom of the page.
+                <br />
+                <br />
+                <a href="https://github.com/benct/tomlin-web/blob/master/CHANGELOG.md" target="_blank" rel="noopener noreferrer">
+                    Version 2.6.1
+                </a>
+            </div>
             <hr />
             <Countdown />
             <hr />
