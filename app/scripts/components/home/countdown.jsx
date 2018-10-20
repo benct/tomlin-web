ff147d1d07ae36fec64f6b75517d7fc611f4dfc5
benct <ben@tomlin.no>
2018-10-20 18:56:52 +0200

    Refactor countdown component

--- app/scripts/components/home/countdown.jsx
+++ app/scripts/components/home/countdown.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default class Countdown extends React.Component {
     constructor(props) {
         super(props);
-        this.calculateCountdown = this.calculateCountdown.bind(this);
+
+        this.countdownTo = 1600000000000;
         this.state = {
-            countdownTo: 1600000000000,
             days: 0,
             hours: 0,
             minutes: 0,
@@ -15,24 +15,25 @@ export default class Countdown extends React.Component {
 
     componentDidMount() {
         this.calculateCountdown();
-        this.interval = setInterval(this.calculateCountdown, 1000);
+        this.interval = setInterval(this.calculateCountdown.bind(this), 1000);
     }
 
     componentWillUnmount() {
         clearInterval(this.interval);
     }
 
-    timeComponent(x, v) {
+    static timeComponent(x, v) {
         return Math.floor(x / v);
     }
 
     calculateCountdown() {
-        const timestamp = (this.state.countdownTo - Date.now()) / 1000;
+        const timestamp = (this.countdownTo - Date.now()) / 1000;
+
         this.setState({
-            days: this.timeComponent(timestamp, 24 * 60 * 60),
-            hours: this.timeComponent(timestamp, 60 * 60) % 24,
-            minutes: this.timeComponent(timestamp, 60) % 60,
-            seconds: this.timeComponent(timestamp, 1) % 60,
+            days: Countdown.timeComponent(timestamp, 24 * 60 * 60),
+            hours: Countdown.timeComponent(timestamp, 60 * 60) % 24,
+            minutes: Countdown.timeComponent(timestamp, 60) % 60,
+            seconds: Countdown.timeComponent(timestamp, 1) % 60,
         });
     }
 
