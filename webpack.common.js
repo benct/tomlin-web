f68434e699d42d38c4115325975e542eb3c8bf53
benct <ben@tomlin.no>
2018-10-27 16:00:29 +0200

    Add @babel/plugin-syntax-dynamic-import

--- webpack.common.js
+++ webpack.common.js
@@ -26,6 +26,7 @@ module.exports = {
                     loader: 'babel-loader',
                     options: {
                         presets: ['@babel/preset-env', '@babel/preset-react'],
+                        plugins: ['@babel/plugin-syntax-dynamic-import'],
                     },
                 },
             },
