391dbdffaa75692d1e9c911cd61b5f9b4bee2c45
benct <ben@tomlin.no>
2018-10-27 16:37:00 +0200

    Make link more descriptive to improve accessibility

--- app/scripts/components/home/home.jsx
+++ app/scripts/components/home/home.jsx
@@ -13,10 +13,9 @@ export default function Home() {
                 Unfortunately, there&apos;s not that much interesting here...
                 <br />
                 <br />
-                This site was developed a while back using React, for the purpose of learning and testing new features, and acquiring basic
-                knowledge about Webpack, Babel, Node and other web development technologies. For more information about me, click&nbsp;
-                <Link to="/about">here</Link>
-                &nbsp;or feel free to follow the social links at the bottom of the page.
+                This site was developed a while back using React, for the purpose of learning and testing new features and acquiring basic
+                knowledge about Webpack, Babel, Node and other web development technologies. More information about me can be found under
+                the <Link to="/about">about</Link> tab, or feel free to follow the social links at the bottom of the page.
                 <br />
                 <br />
                 <a href="https://github.com/benct/tomlin-web/blob/master/CHANGELOG.md" target="_blank" rel="noopener noreferrer">
