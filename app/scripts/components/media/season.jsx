d76e18c7c5fb52b3bc7198187d35277f6846aa9d
benct <ben@tomlin.no>
2018-11-03 02:26:57 +0100

    Add episode overview overlay

--- app/scripts/components/media/season.jsx
+++ app/scripts/components/media/season.jsx
@@ -13,11 +13,17 @@ class Season extends React.PureComponent {
 
         this.state = {
             showEpisodes: false,
+            title: null,
+            overview: null,
         };
     }
 
     toggleEpisodes() {
         this.setState({ showEpisodes: !this.state.showEpisodes });
+    }
+
+    setOverview(title, overview) {
+        this.setState({ title, overview });
     }
 
     setAllSeen() {
@@ -30,7 +36,11 @@ class Season extends React.PureComponent {
         return this.props.data.episodes.map(episode => (
             <React.Fragment key={`episode${episode.id}`}>
                 <span className="text-small pll">{episode.episode}</span>
-                <span className="text-small truncate">{episode.title}</span>
+                <button
+                    className="button-blank text-small text-left truncate"
+                    onClick={this.setOverview.bind(this, episode.title, episode.overview)}>
+                    {episode.title}
+                </button>
                 <span className="text-small text-right">{episode.release_date ? formatDate(episode.release_date, 'MMM do') : null}</span>
                 <button
                     className="button-blank mrm"
@@ -63,7 +73,17 @@ class Season extends React.PureComponent {
                     </button>
                     <span className="text-small plm">{formatDate(this.props.data.release_date, '(MMM do, YYY)')}</span>
                 </span>
-                {this.state.showEpisodes ? <div className="media-overlay-episodes">{this.renderEpisodes()}</div> : null}
+                {this.state.showEpisodes ? (
+                    <div className="media-overlay-episodes">
+                        {this.renderEpisodes()}
+                        {this.state.overview ? (
+                            <div className="media-overlay-episode-overview shadow" role="dialog" onClick={this.setOverview.bind(this)}>
+                                <span>{this.state.title}</span>
+                                <span className="text-small">{this.state.overview}</span>
+                            </div>
+                        ) : null}
+                    </div>
+                ) : null}
             </>
         );
     }
