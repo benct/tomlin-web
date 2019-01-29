3ba63012f1b4789e5b64fb650e74b1722764d70f
benct <ben@tomlin.no>
2019-01-29 23:01:30 +0100

    Bump version to 2.8.1

--- app/scripts/components/home/home.jsx
+++ app/scripts/components/home/home.jsx
@@ -16,7 +16,7 @@ export default function Home() {
                 <br />
                 <br />
                 <a href="https://github.com/benct/tomlin-web/blob/master/CHANGELOG.md" target="_blank" rel="noopener noreferrer">
-                    Version 2.8.0
+                    Version 2.8.1
                 </a>
             </div>
             <hr />
