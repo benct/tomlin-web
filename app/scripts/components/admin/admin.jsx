3e350023017f698dff20e40d4c1a722bce8aa750
benct <ben@tomlin.no>
2019-03-02 22:32:25 +0100

    Add more log count options

--- app/scripts/components/admin/admin.jsx
+++ app/scripts/components/admin/admin.jsx
@@ -51,6 +51,8 @@ class Admin extends React.PureComponent {
                         <option value={25}>25</option>
                         <option value={50}>50</option>
                         <option value={100}>100</option>
+                        <option value={250}>250</option>
+                        <option value={500}>500</option>
                     </select>
                 </div>
                 <hr />
