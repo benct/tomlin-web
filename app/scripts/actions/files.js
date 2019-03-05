81839e3740844694753725058ac17771d3c36be8
benct <ben@tomlin.no>
2019-03-05 16:45:33 +0100

    Use new backend files service

--- app/scripts/actions/files.js
+++ app/scripts/actions/files.js
@@ -18,7 +18,7 @@ actions.setUploading = makeAction('FILES/SET_UPLOADING', 'uploading');
 actions.setDirectory = makeAction('FILES/CHANGE_DIR', (state, { payload }) => Object.assign({}, state, { cwd: payload, focused: null }));
 
 actions.refresh = cwd => (dispatch, getState) =>
-    post({ service: 'fs', action: 'ls', path: cwd || getState().files.cwd })
+    post({ service: 'files', action: 'ls', path: cwd || getState().files.cwd })
         .then(data => dispatch(actions.setContent(data)))
         .catch(() => dispatch(baseActions.showToast('Could not fetch content...')));
 
@@ -37,7 +37,7 @@ actions.changeDirectory = dirname => (dispatch, getState) => {
 actions.createDirectory = () => (dispatch, getState) => {
     const name = prompt('Enter name of new folder:');
     if (name) {
-        post({ service: 'fs', action: 'mkdir', path: `${getState().files.cwd}/${name}` })
+        post({ service: 'files', action: 'mkdir', path: `${getState().files.cwd}/${name}` })
             .then(() => dispatch(actions.refresh()))
             .catch(() => dispatch(baseActions.showToast('Could not create directory...')));
     }
@@ -46,7 +46,7 @@ actions.createDirectory = () => (dispatch, getState) => {
 actions.rename = item => (dispatch, getState) => {
     const name = prompt('Enter new name of file:', item.name);
     if (name) {
-        post({ service: 'fs', action: 'mv', path: `${getState().files.cwd}/${item.name}`, name: name })
+        post({ service: 'files', action: 'mv', path: `${getState().files.cwd}/${item.name}`, name: name })
             .then(() => dispatch(actions.refresh()))
             .catch(() => dispatch(baseActions.showToast(`Could not rename ${item.dir ? 'directory' : 'file'}...`)));
     }
@@ -55,7 +55,7 @@ actions.rename = item => (dispatch, getState) => {
 actions.delete = item => (dispatch, getState) => {
     if (confirm(`Are you sure you want to delete ${item.name}?`)) {
         const action = item.dir ? 'rmdir' : 'rm';
-        post({ service: 'fs', action: action, path: `${getState().files.cwd}/${item.name}` })
+        post({ service: 'files', action: action, path: `${getState().files.cwd}/${item.name}` })
             .then(() => dispatch(actions.refresh()))
             .catch(() => dispatch(baseActions.showToast(`Could not delete ${item.dir ? 'directory' : 'file'}...`)));
     }
@@ -64,7 +64,7 @@ actions.delete = item => (dispatch, getState) => {
 actions.upload = data => (dispatch, getState) => {
     dispatch(actions.setUploading(true));
 
-    post({ service: 'fs', action: 'up', path: getState().files.cwd }, data)
+    post({ service: 'files', action: 'up', path: getState().files.cwd }, data)
         .then(() => {
             dispatch(actions.setUploading(false));
             dispatch(actions.refresh());
