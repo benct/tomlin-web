17f6bd9c46a3745fe1f09a15c06a8ed404137356
benct <ben@tomlin.no>
2019-05-08 01:19:05 +0200

    Project support for TypeScript

--- webpack.common.js
+++ webpack.common.js
@@ -15,10 +15,15 @@ module.exports = {
         publicPath: '/',
     },
     resolve: {
-        extensions: ['.js', '.jsx'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
     },
     module: {
         rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'ts-loader'],
+            },
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
