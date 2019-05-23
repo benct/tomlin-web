1cec59e1b47d1a22b122354800f153d09b8c9f7b
benct <ben@tomlin.no>
2019-05-23 15:08:33 +0200

    Improve home functional component

--- app/scripts/components/home/Home.tsx
+++ app/scripts/components/home/Home.tsx
@@ -5,26 +5,26 @@ import Countdown from './Countdown';
 import State from './State';
 import Quote from './Quote';
 
-export default function Home(): React.ReactElement {
-    return (
-        <>
-            <div className="wrapper text">
-                <h2 className="home-title color-primary mbm">Welcome</h2>
-                This site was developed a while back using React, for the purpose of learning and testing new features and acquiring basic
-                knowledge about Webpack, Babel, Node and other web development technologies. More information about me can be found under
-                the <Link to="/about">about</Link> tab, or feel free to follow the social links at the bottom of the page.
-                <br />
-                <br />
-                <a href="https://github.com/benct/tomlin-web/blob/master/CHANGELOG.md" target="_blank" rel="noopener noreferrer">
-                    Version 2.9.3
-                </a>
-            </div>
-            <hr />
-            <State />
-            <hr />
-            <Countdown day={3} month={8} year={2019} hour={13} title="Countdown to something..." icon="wedding" />
-            <hr />
-            <Quote />
-        </>
-    );
-}
+const Home: React.FC = (): React.ReactElement => (
+    <>
+        <div className="wrapper text">
+            <h2 className="home-title color-primary mbm">Welcome</h2>
+            This site was developed a while back using React, for the purpose of learning and testing new features and acquiring basic
+            knowledge about Webpack, Babel, Node and other web development technologies. More information about me can be found under the{' '}
+            <Link to="/about">about</Link> tab, or feel free to follow the social links at the bottom of the page.
+            <br />
+            <br />
+            <a href="https://github.com/benct/tomlin-web/blob/master/CHANGELOG.md" target="_blank" rel="noopener noreferrer">
+                Version 2.9.3
+            </a>
+        </div>
+        <hr />
+        <State />
+        <hr />
+        <Countdown day={3} month={8} year={2019} hour={13} title="Countdown to something..." icon="wedding" />
+        <hr />
+        <Quote />
+    </>
+);
+
+export default React.memo(Home);
