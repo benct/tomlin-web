f34ad74eecb82e7f473af54f95c0f926d9951524
benct <ben@tomlin.no>
2019-07-08 23:52:37 +0200

    Bump version to 2.10.1

--- app/scripts/components/home/Home.tsx
+++ app/scripts/components/home/Home.tsx
@@ -14,7 +14,7 @@ const Home: React.FC = (): React.ReactElement => (
                 <Link to="/about">here</Link> or follow the social media links at the bottom of the page.
             </p>
             <a href="https://github.com/benct/tomlin-web/blob/master/CHANGELOG.md" target="_blank" rel="noopener noreferrer">
-                Version 2.10.0
+                Version 2.10.1
             </a>
         </div>
         <hr />
