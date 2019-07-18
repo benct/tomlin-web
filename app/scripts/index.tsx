58a159ac991fedf26483eb1e46a6e7ac72e7b09c
benct <ben@tomlin.no>
2019-07-19 01:43:24 +0200

    Remove unused PureCSS module

--- app/scripts/index.tsx
+++ app/scripts/index.tsx
@@ -9,7 +9,6 @@ import store from './redux/store';
 import App from './components/App';
 
 import 'purecss/build/pure-min.css';
-import 'purecss/build/grids-responsive-min.css';
 
 // Static assets
 import '../.htaccess';
