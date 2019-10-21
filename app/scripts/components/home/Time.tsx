bb04b8a33f335145e47c488b85c7284ad4ea5fe0
benct <ben@tomlin.no>
2019-10-21 15:55:46 +0200

    Refactor time component using useRef hook

--- app/scripts/components/home/Time.tsx
+++ app/scripts/components/home/Time.tsx
@@ -8,18 +8,16 @@ interface TimeProps {
 const Time: React.FC<TimeProps> = ({ locale, timeZone }) => {
     const now = (): string => new Date().toLocaleTimeString(locale, { timeZone });
 
+    const interval = React.useRef<number>(0);
     const [time, setTime] = React.useState<string>(now());
 
-    const tick = (): void => {
-        setTime(now());
-    };
-
     React.useEffect(() => {
+        const tick = (): void => setTime(now());
+
         tick();
+        interval.current = window.setInterval(tick, 1000);
 
-        const interval = window.setInterval(tick, 1000);
-
-        return (): void => window.clearInterval(interval);
+        return (): void => window.clearInterval(interval.current);
     }, []);
 
     return <>{time}</>;
