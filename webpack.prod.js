d121e33e29ec436aadff03ba07fdb74d86202ccf
benct <ben@tomlin.no>
2019-11-04 00:06:13 +0000

    Simplify webpack css module config

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,12 +8,7 @@ module.exports = merge(common, {
         rules: [
             {
                 test: /\.css$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    'css-loader',
-                ],
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
         ],
     },
