9c621cb1a32bc48b24bae17c3825621dce74c024
benct <ben@tomlin.no>
2019-11-26 00:06:36 +0100

    Secondary color on social icons

--- app/scripts/components/page/Social.tsx
+++ app/scripts/components/page/Social.tsx
@@ -12,35 +12,35 @@ const Social: React.FC<SocialProps> = ({ circle }): React.ReactElement => (
             {circle ? (
                 <img src={require(`../../../images/social/circle-github.svg`)} alt="GitHub" />
             ) : (
-                <Icon path={mdiGithubCircle} size={1} title="GitHub" className="valign-middle" />
+                <Icon path={mdiGithubCircle} size={1} title="GitHub" className="valign-middle color-secondary" />
             )}
         </a>
         <a href="https://www.facebook.com/ben.c.tomlin" target="_blank" rel="noopener noreferrer">
             {circle ? (
                 <img src={require(`../../../images/social/circle-facebook.svg`)} alt="Facebook" />
             ) : (
-                <Icon path={mdiFacebook} size={1} title="Facebook" className="valign-middle" />
+                <Icon path={mdiFacebook} size={1} title="Facebook" className="valign-middle color-secondary" />
             )}
         </a>
         <a href="https://www.instagram.com/benctomlin" target="_blank" rel="noopener noreferrer">
             {circle ? (
                 <img src={require(`../../../images/social/circle-instagram.svg`)} alt="Instagram" />
             ) : (
-                <Icon path={mdiInstagram} size={1} title="Instagram" className="valign-middle" />
+                <Icon path={mdiInstagram} size={1} title="Instagram" className="valign-middle color-secondary" />
             )}
         </a>
         <a href="https://www.linkedin.com/in/bentomlin" target="_blank" rel="noopener noreferrer">
             {circle ? (
                 <img src={require(`../../../images/social/circle-linkedin.svg`)} alt="Linkedin" />
             ) : (
-                <Icon path={mdiLinkedin} size={1} title="LinkedIn" className="valign-middle" />
+                <Icon path={mdiLinkedin} size={1} title="LinkedIn" className="valign-middle color-secondary" />
             )}
         </a>
         <a href="mailto:ben@tomlin.no" target="_blank" rel="noopener noreferrer">
             {circle ? (
                 <img src={require(`../../../images/social/circle-email.svg`)} alt="E-mail" />
             ) : (
-                <Icon path={mdiAt} size={1} title="E-mail" className="valign-middle" />
+                <Icon path={mdiAt} size={1} title="E-mail" className="valign-middle color-secondary" />
             )}
         </a>
     </div>
