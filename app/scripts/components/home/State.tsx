01b7f4fbfb7a67c6b408ceb466ab9eb479e6ec8a
benct <ben@tomlin.no>
2019-11-26 01:56:08 +0100

    Add state icons as inline SVGs

--- app/scripts/components/home/State.tsx
+++ app/scripts/components/home/State.tsx
@@ -4,9 +4,19 @@ import Icon from '@mdi/react';
 import { mdiInformationOutline, mdiLoading } from '@mdi/js';
 
 import { DefaultState, HomeState, ThunkDispatchProp } from '../../interfaces';
-
 import { getHomeState } from '../../actions/base';
 
+import {
+    BathroomIcon,
+    BedroomIcon,
+    ComputerIcon,
+    KitchenIcon,
+    LivingRoomIcon,
+    OfficeIcon,
+    StateIconProps,
+    StoreRoomIcon,
+    TvIcon,
+} from './StateIcons';
 import Time from './Time';
 
 type HomeStateProps = HomeState & Pick<DefaultState, 'loading'>;
@@ -16,10 +26,10 @@ const State: React.FC<HomeStateProps & ThunkDispatchProp> = props => {
         props.dispatch(getHomeState());
     }, []);
 
-    const renderState = (id: string, value?: number, text?: string, unit = '°C'): React.ReactElement => (
+    const renderState = (Icon: React.FC<StateIconProps>, value?: number, text?: string, unit = '°C'): React.ReactElement => (
         <div className="home-state" data-tooltip={text}>
             <div className="text-right">
-                <img className="valign-middle" src={require(`../../../images/icon/${id}.svg`)} alt={text} width={40} height={40} />
+                <Icon className="valign-middle" alt={text} width={40} height={40} />
             </div>
             <div className="text-left">
                 <span className="home-value">{value ? Number(value).toFixed(1) : '-'}</span>
@@ -47,14 +57,14 @@ const State: React.FC<HomeStateProps & ThunkDispatchProp> = props => {
                         />
                     </span>
                 </div>
-                <div className="color-secondary">
+                <div>
                     <span className="home-unit">Oslo</span>
                     <br />
                     <span className="home-value">
                         <Time locale="nb-NO" timeZone="Europe/Oslo" />
                     </span>
                 </div>
-                <div className="color-secondary ptm">
+                <div className="ptm">
                     <img
                         className="valign-middle phm"
                         src={require(`../../../images/icon/${props.day ? 'day' : 'night'}.svg`)}
@@ -68,15 +78,15 @@ const State: React.FC<HomeStateProps & ThunkDispatchProp> = props => {
                     </div>
                 </div>
             </div>
-            <div className="home-sensors color-secondary">
-                {renderState('livingroom', props.temperature.livingroom, 'Living room temperature')}
-                {renderState('office', props.temperature.office, 'Office temperature')}
-                {renderState('bedroom', props.temperature.bedroom, 'Bedroom temperature')}
-                {renderState('storeroom', props.temperature.storeroom, 'Storage room temperature')}
-                {renderState('kitchen', props.temperature.kitchen, 'Kitchen temperature')}
-                {renderState('computer', props.consumption.pc, 'PC consumption [30d]', 'kWh')}
-                {renderState('bathroom', props.temperature.bathroom, 'Bathroom temperature')}
-                {renderState('tv', props.consumption.tv, 'TV/Sound consumption [30d]', 'kWh')}
+            <div className="home-sensors">
+                {renderState(LivingRoomIcon, props.temperature.livingroom, 'Living room temperature')}
+                {renderState(OfficeIcon, props.temperature.office, 'Office temperature')}
+                {renderState(BedroomIcon, props.temperature.bedroom, 'Bedroom temperature')}
+                {renderState(StoreRoomIcon, props.temperature.storeroom, 'Storage room temperature')}
+                {renderState(KitchenIcon, props.temperature.kitchen, 'Kitchen temperature')}
+                {renderState(ComputerIcon, props.consumption.pc, 'PC consumption [30d]', 'kWh')}
+                {renderState(BathroomIcon, props.temperature.bathroom, 'Bathroom temperature')}
+                {renderState(TvIcon, props.consumption.tv, 'TV/Sound consumption [30d]', 'kWh')}
             </div>
         </div>
     );
