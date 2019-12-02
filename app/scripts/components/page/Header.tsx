cb2ddf255818e363bd70fb48ae3ef155163a9093
benct <ben@tomlin.no>
2019-12-02 20:39:26 +0100

    Remove global margin modification on header elements

--- app/scripts/components/page/Header.tsx
+++ app/scripts/components/page/Header.tsx
@@ -9,7 +9,7 @@ import Navigation from './Navigation';
 
 const Header: React.FC<ThunkDispatchProp> = ({ dispatch }): React.ReactElement => (
     <header>
-        <h1 className="site-title no-select">Tomlin</h1>
+        <h1 className="site-title no-select man">Tomlin</h1>
         <Navigation type="simple" />
         <Navigation type="full" />
         <button className="menu-overlay button-blank hide-gt480" aria-label="Menu" onClick={(): Action => dispatch(actions.toggleMenu())}>
