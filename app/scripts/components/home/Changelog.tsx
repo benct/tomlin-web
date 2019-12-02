f2a7621560963e0e2ee48c66e050e7c0b7d865e0
benct <ben@tomlin.no>
2019-12-02 20:40:21 +0100

    Add changelog view modal overlay

---
+++ app/scripts/components/home/Changelog.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Icon from '@mdi/react';
+import { mdiCloseCircleOutline } from '@mdi/js';
+
+import content from '../../../../CHANGELOG.md';
+import Modal from '../page/Modal';
+
+interface ChangelogProps {
+    close: () => void;
+}
+
+const Changelog: React.FC<ChangelogProps> = ({ close }) => (
+    <Modal close={close}>
+        <div className="overlay-modal-content changelog" dangerouslySetInnerHTML={{ __html: content }} />
+        <div className="border-top ptm">
+            <a href="https://github.com/benct/tomlin-web/blob/master/CHANGELOG.md" target="_blank" rel="noopener noreferrer">
+                View on GitHub
+            </a>
+            <button className="button-blank float-right" data-tooltip="Close" onClick={close}>
+                <Icon path={mdiCloseCircleOutline} size="28px" title="Close" />
+            </button>
+        </div>
+    </Modal>
+);
+
+export default React.memo(Changelog);
--- app/scripts/components/home/Home.tsx
+++ app/scripts/components/home/Home.tsx
@@ -11,41 +11,49 @@ import State from './State';
 import Quote from './Quote';
 
 import '../../../styles/home.css';
+import Loading from '../page/Loading';
 
 type HomeProps = Pick<DefaultState, 'theme' | 'settings'> & ThunkDispatchProp;
 
-const Home: React.FC<HomeProps> = ({ theme, settings, dispatch }): React.ReactElement => (
-    <>
-        <div className="wrapper text">
-            <h2 className="home-title color-primary mbm">Hello</h2>
-            <p className="limit-width mvl">
-                This site is just a personal website project I doodle with from time to time. See more about me{' '}
-                <Link to="/about">here</Link> or follow the social media links at the bottom of the page.
-            </p>
-            <div className="home-info text-small">
-                Theme
-                <button className="input input-small" onClick={(): Action => dispatch(actions.toggleTheme())}>
-                    {theme}
-                </button>
+const Changelog = React.lazy((): Promise<any> => import('./Changelog'));
+
+const Home: React.FC<HomeProps> = ({ theme, settings, dispatch }): React.ReactElement => {
+    const [showChangelog, setShowChangelog] = React.useState(false);
+
+    return (
+        <>
+            <div className="wrapper text">
+                <h2 className="home-title color-primary man mbm">Hello</h2>
+                <p className="limit-width mvl">
+                    This site is just a personal website project I doodle with from time to time. See more about me{' '}
+                    <Link to="/about">here</Link> or follow the social media links at the bottom of the page.
+                </p>
+                <div className="home-info text-small">
+                    Theme
+                    <button className="input input-small" onClick={(): Action => dispatch(actions.toggleTheme())}>
+                        {theme}
+                    </button>
+                </div>
+                <div className="home-info text-small">
+                    Version
+                    <button className="input input-small" onClick={(): void => setShowChangelog(!showChangelog)}>
+                        2.13.1
+                    </button>
+                </div>
             </div>
-            <div className="home-info text-small">
-                Version
-                <a
-                    className="input input-small"
-                    href="https://github.com/benct/tomlin-web/blob/master/CHANGELOG.md"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    2.13.1
-                </a>
-            </div>
-        </div>
-        <hr />
-        <State />
-        <hr />
-        <Countdown title="Countdown to something..." timestamp={settings.countdownTarget} icon={settings.countdownIcon} />
-        <hr />
-        <Quote />
-    </>
-);
+            <hr />
+            <State />
+            <hr />
+            <Countdown title="Countdown to something..." timestamp={settings.countdownTarget} icon={settings.countdownIcon} />
+            <hr />
+            <Quote />
+            {showChangelog && (
+                <React.Suspense fallback={<Loading isLoading className="wrapper" />}>
+                    <Changelog close={(): void => setShowChangelog(false)} />
+                </React.Suspense>
+            )}
+        </>
+    );
+};
 
 export default connect((state: DefaultState) => ({ theme: state.theme, settings: state.settings }))(React.memo(Home));
