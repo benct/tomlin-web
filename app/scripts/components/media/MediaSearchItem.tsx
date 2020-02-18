86ac9a78f30841036727fef35e674fb118bcefca
benct <ben@tomlin.no>
2020-02-19 00:50:16 +0100

    Simplify add/remove button on media search items

--- app/scripts/components/media/MediaSearchItem.tsx
+++ app/scripts/components/media/MediaSearchItem.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ISO6391 from 'iso-639-1';
-import Icon from '@mdi/react';
-import { mdiPlus } from '@mdi/js';
 
 import { MediaSearchItemEntry } from '../../interfaces';
 import { formatDate } from '../../util/formatting';
@@ -22,12 +20,20 @@ const MediaSearchItem: React.FC<MediaSearchItemProps> = props => {
     const title = props.data.title ?? props.data.name;
     const originalTitle = props.data.original_title ?? props.data.original_name;
     const release = props.data.release_date ?? props.data.first_air_date;
-    const text = props.stored ? 'Remove' : 'Add';
-    const change = props.stored ? props.remove : props.add;
+    const language = ISO6391.getName(props.data.original_language);
 
     const validLanguage = (code: string): boolean => code === 'en' || code === 'no' || code === 'nb';
 
     const toggleOverview = (): void => setOverview(!overview);
+
+    const renderButton = (className?: string): React.ReactElement => (
+        <button
+            className={`input input-small ${className ?? ''}`}
+            onClick={props.stored ? props.remove : props.add}
+            style={{ width: '70px' }}>
+            {props.stored ? 'Remove' : 'Add'}
+        </button>
+    );
 
     return (
         <div className="media-item pvm">
@@ -38,13 +44,6 @@ const MediaSearchItem: React.FC<MediaSearchItemProps> = props => {
                     onError={(event: React.InvalidEvent<HTMLImageElement>): void => (event.target.src = defaultPoster)}
                     onClick={toggleOverview}
                 />
-                <div
-                    className="media-poster-overlay"
-                    role="dialog"
-                    style={{ opacity: overview ? 1 : 0, zIndex: overview ? 10 : -1 }}
-                    onClick={overview ? change : undefined}>
-                    <Icon path={mdiPlus} size="48px" color="white" rotate={props.stored ? 45 : 0} title={text} />
-                </div>
             </div>
             <h3 className="media-title color-primary truncate man" onClick={toggleOverview}>
                 {title} {formatDate(release, '(yyyy)')}
@@ -52,7 +51,7 @@ const MediaSearchItem: React.FC<MediaSearchItemProps> = props => {
             <div className="media-data text-small">
                 {title !== originalTitle ? <div className="text-small italic">Orig: {originalTitle}</div> : null}
                 <span className={validLanguage(props.data.original_language) ? 'color-success' : 'color-warn'}>
-                    {ISO6391.getName(props.data.original_language)}
+                    {language !== '' ? language : 'Unknown'}
                 </span>
                 {props.data.vote_average ? (
                     <span>
@@ -73,18 +72,10 @@ const MediaSearchItem: React.FC<MediaSearchItemProps> = props => {
                     <button className="button-blank" onClick={props.imdb}>
                         <img src={require(`../../../images/icon/imdb.svg`)} alt="IMDb" width={48} />
                     </button>
-                    <button className="button-icon button-icon-text float-right mtm" onClick={change}>
-                        <span>{text}</span>
-                        <Icon path={mdiPlus} size="28px" rotate={props.stored ? 45 : 0} title={text} />
-                    </button>
+                    {renderButton('float-right mtm')}
                 </div>
             </div>
-            <div className="media-actions">
-                <button className="button-icon button-icon-text pan" onClick={change}>
-                    <span>{text}</span>
-                    <Icon path={mdiPlus} size="28px" rotate={props.stored ? 45 : 0} title={text} />
-                </button>
-            </div>
+            <div className="media-actions">{renderButton()}</div>
             <div className="media-external">
                 <img
                     className="pointer"
