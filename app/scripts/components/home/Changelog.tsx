16a02144429e538aa2e6a0bc8ef082df13206339
benct <ben@tomlin.no>
2020-02-19 01:06:40 +0100

    Add github icon to changelog modal

--- app/scripts/components/home/Changelog.tsx
+++ app/scripts/components/home/Changelog.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Icon from '@mdi/react';
-import { mdiCloseCircleOutline } from '@mdi/js';
+import { mdiCloseCircleOutline, mdiGithubCircle } from '@mdi/js';
 
 import content from '../../../../CHANGELOG.md';
 import Modal from '../page/Modal';
@@ -14,9 +14,10 @@ const Changelog: React.FC<ChangelogProps> = ({ close }) => (
         <div className="overlay-modal-content changelog" dangerouslySetInnerHTML={{ __html: content }} />
         <div className="border-top ptm">
             <a href="https://github.com/benct/tomlin-web/blob/master/CHANGELOG.md" target="_blank" rel="noopener noreferrer">
-                View on GitHub
+                <Icon path={mdiGithubCircle} size="28px" title="View on GitHub" className="valign-middle" />
+                <span className="valign-middle mlm">View on GitHub</span>
             </a>
-            <button className="button-blank float-right" onClick={close}>
+            <button className="button-icon pan float-right" onClick={close}>
                 <Icon path={mdiCloseCircleOutline} size="28px" title="Close" />
             </button>
         </div>
