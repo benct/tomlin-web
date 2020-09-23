de30b30143a14e2685829cd23467c879563318ee
benct <ben@tomlin.no>
2020-09-23 18:54:36 +0200

    Simplify suspended route component

--- app/scripts/components/route/Suspended.tsx
+++ app/scripts/components/route/Suspended.tsx
@@ -3,35 +3,25 @@ import { Redirect, Route, RouteProps } from 'react-router-dom';
 
 import Loading from '../page/Loading';
 
-type SuspendedComponent = typeof React.Component | React.FunctionComponent | React.LazyExoticComponent<any>;
-
-interface SuspendedProps {
-    path: string;
-    component: SuspendedComponent;
-    requireAuth?: boolean;
-    extraProps?: Record<string, string>;
+interface SuspendedRouteProps {
+    authentication?: boolean;
 }
 
 const isLoggedIn = (): boolean => !!localStorage.getItem('token');
 
-const SuspendedRoute: React.FC<SuspendedProps> = ({ path, component: Component, requireAuth, extraProps }): React.ReactElement => (
-    <Route
-        path={path}
-        render={(props: RouteProps): React.ReactElement =>
-            !requireAuth || isLoggedIn() ? (
-                <React.Suspense fallback={<Loading isLoading className="wrapper min-height" />}>
-                    <Component {...props} {...extraProps} />
-                </React.Suspense>
-            ) : (
+export default class SuspendedRoute extends Route<SuspendedRouteProps & RouteProps> {
+    render(): React.ReactNode {
+        if (this.props.authentication && !isLoggedIn()) {
+            return (
                 <Redirect
                     to={{
                         pathname: '/login',
-                        state: { from: props.location },
+                        state: { from: this.props.location },
                     }}
                 />
-            )
+            );
         }
-    />
-);
 
-export default SuspendedRoute;
+        return <React.Suspense fallback={<Loading isLoading className="wrapper min-height" />}>{super.render()}</React.Suspense>;
+    }
+}
