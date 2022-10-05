af70e3f65d44809be9a4e68b2b2ac870b5da58e5
benct <ben@tomlin.no>
2022-10-05 02:53:36 +0200

    Add Tailwind dependencies and configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,21 +4,7 @@ import { AppContextProvider } from '../data/context';
 import { Layout } from '../components/Layout';
 
 import '../styles/globals.css';
-import '../styles/content.css';
-import '../styles/header.css';
-import '../styles/footer.css';
-import '../styles/form.css';
 import '../styles/util.css';
-import '../styles/themes/default.css';
-import '../styles/themes/midnight.css';
-
-// TODO modularize css
-import '../styles/home.css';
-import '../styles/about.css';
-import '../styles/media.css';
-import '../styles/admin.css';
-import '../styles/files.css';
-import '../styles/error.css';
 
 const App = ({ Component, pageProps }: AppProps) => (
     <AppContextProvider>
