bd6f6c048efd7e31c6f2e307277d0313b5b932f7
benct <ben@tomlin.no>
2023-09-22 00:32:33 +0200

    Fix loading text color

--- src/components/page/Loading.tsx
+++ src/components/page/Loading.tsx
@@ -12,7 +12,7 @@ export const Loading = ({ isLoading, text, className, children }: PropsWithChild
     isLoading ? (
         <div className={`flex justify-center items-center gap-x-16 py-16 ${className ?? ''}`}>
             <Icon path={mdiLoading} size={1} title="Loading" spin={1} id="loading-icon" />
-            <span className="italic">{text ?? 'Loading...'}</span>
+            <span className="text-primary dark:text-primary-dark italic">{text ?? 'Loading...'}</span>
         </div>
     ) : (
         <>{children}</>
